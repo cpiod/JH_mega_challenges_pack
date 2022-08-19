c6e337373a92405744a9c177cb0bc5a72ffc18d7
Pierre-François Gimenez <pierre-francois.gimenez@centralesupelec.fr>
2022-08-19 19:19:58 +0200

    Angel of Infestation and the Hunt

--- main.lua
+++ main.lua
@@ -91,6 +91,14 @@ register_blueprint "chall_event_volatile_storage"
                 })
             end
         ]],
+        on_cleared = [[
+            function ( self, level )
+                local ui_event = gtk.get_event_ui( self )
+                if ui_event and ui_event.active then
+                    ui_event.active = false
+                end
+            end
+        ]],
     }
 }
 
@@ -116,6 +124,186 @@ register_blueprint "challenge_volatility"
         ]],
     },
 }
+
+
+register_blueprint "chall_event_infestation"
+{
+    text = {
+		name          = "Infestation",
+        intro_fiend   = "There's a foul stench in the air...",
+        intro_reaver  = "You hear the shrieking of reavers...",
+        intro_kerberos= "The dogs have been unleashed...",
+        intro_demon   = "You shiver from dread...",
+	},
+    callbacks = {
+        on_create = [[
+            function ( self )
+                local level = world:get_level()
+                level.attributes.bulk_mod = 0.0
+            end
+        ]],
+        on_enter_level = [[
+            function ( self, level, entity, reenter )
+                if reenter then return end
+                local linfo    = level.level_info
+                local bulk_max = generator.std_exp_value( linfo )
+                local dlevel   = linfo.dlevel - math.random(2)
+                local params = {
+                    list  = event_infestation_beings,
+                    mod   = {},
+                    level = dlevel,
+                    safe  = linfo.entry,
+                }
+
+                if dlevel < 9 then
+                    ui:set_hint( "{R"..self.text.intro_fiend.."}", 1001, 0 )
+                    bulk_max = bulk_max - generator.spawn_exalted( level, 1, { override = {"exalted_fiend"}, safe  = linfo.entry, } )
+                elseif dlevel < 16 then
+                    ui:set_hint( "{R"..self.text.intro_reaver.."}", 1001, 0 )
+                    bulk_max = bulk_max - generator.spawn_exalted( level, 1, { override = {"exalted_reaver"}, safe  = linfo.entry, } )
+                elseif dlevel < 21 then
+                    ui:set_hint( "{R"..self.text.intro_kerberos.."}", 1001, 0 )
+                    bulk_max = bulk_max - generator.spawn_exalted( level, 1, { override = {"exalted_kerberos"}, safe  = linfo.entry, } )
+                else
+                    ui:set_hint( "{R"..self.text.intro_demon.."}", 1001, 0 )
+                    bulk_max = bulk_max - generator.spawn_exalted( level, 1, { override = {"exalted_reaver"}, safe  = linfo.entry, } )
+                end
+                if level.level_info.depth < 3 then
+                    bulk_max = math.ceil( bulk_max * 0.6 )
+                end
+                generator.spawn_enemies( level, bulk_max, params )
+            end    
+        ]],
+        on_create_entity = [[
+            function ( self, level, entity )
+                if entity.stack and entity.stack.amount > 1 then
+                    entity.stack.amount = math.min( math.ceil( 3 * entity.stack.amount ), entity.stack.max )
+                end
+            end
+        ]],
+        on_cleared = [[
+            function ( self, level )
+                local ui_event = gtk.get_event_ui( self )
+                if ui_event and ui_event.active then
+                    ui_event.active = false
+                    ui:set_hint( "{R"..self.text.complete.."}", 1001, 0 )
+                end
+            end
+        ]],
+    }
+}
+
+
+register_blueprint "challenge_infestation"
+{
+    text = {
+        name   = "Angel of Infestation",
+        desc   = "A relaxing experience, with no zombies, no CRI, no bots.\n\nRating   : {GEASY}",
+        rating = "EASY",
+        abbr   = "AoIn",
+        letter = "I",
+    },
+    challenge = {
+        type      = "challenge",
+    },
+    callbacks = {
+        on_create = [[
+            function( self, player )
+                for i,linfo in ipairs( world.data.level ) do
+                    linfo.event = "chall_event_infestation"
+                end
+            end
+        ]],
+    },
+}
+
+
+register_blueprint "chall_event_hunt"
+{
+    text = {
+		name      = "The Hunt",
+        intro     = "The hunt will start soon...",
+        start     = "The hunt is on!",
+	},
+    callbacks = {
+        on_create = [[
+            function ( self )
+                local level = world:get_level()
+                level.attributes.bulk_mod = 0.8
+            end
+        ]],
+        on_timer = [[
+			function ( self, first )
+                if first then return 100 end
+                local level   = world:get_level()
+                if level.level_info.enemies <= 0 then
+                    return 0
+                end
+                local ui_event = gtk.get_event_ui( self )
+                local counter = ui_event.counter
+                counter = counter - 1 
+                ui_event.counter = counter
+
+                if counter <= 0 then
+                    ui:set_hint( "{R"..self.text.start.."}", 1001, 0 )
+
+                    for e in level:entities() do
+                        if e.data and e.data.ai and e.data.ai.group ~= "player" then
+                            e.target.entity = world:get_player()
+                            e.data.ai.state = "find"
+                        end
+                    end
+                    return 0
+                end
+                return 100
+            end
+        ]],
+        on_enter_level = [[
+            function ( self, level, entity, reenter )
+                if reenter then return end
+                local ui_event = gtk.get_event_ui( self )
+                ui_event.counter = 60
+                ui_event.style   = 1
+                ui:set_hint( "{R"..self.text.intro.."}", 1001, 0 )
+                world:add_history( "event", self, level )
+            end    
+        ]],
+        on_cleared = [[
+            function ( self, level )
+                local ui_event = gtk.get_event_ui( self )
+                if ui_event.active then
+                    ui_event.active = false
+                end
+            end
+        ]],
+    },
+}
+
+
+register_blueprint "challenge_hunt"
+{
+    text = {
+        name   = "Angel of the Hunt",
+        desc   = "They know you are coming. They are waiting for you.\n\nRating   : {GEASY}",
+        rating = "EASY",
+        abbr   = "AoHu",
+        letter = "H",
+    },
+    challenge = {
+        type      = "challenge",
+    },
+    callbacks = {
+        on_create = [[
+            function( self, player )
+                for i,linfo in ipairs( world.data.level ) do
+                    linfo.event = "chall_event_hunt"
+                end
+            end
+        ]],
+    },
+}
+
+
 
 register_blueprint "runtime_bullet_rain"
 {
