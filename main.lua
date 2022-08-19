cf62defaa4c481f8e1edbf122449310f0d13f3d3
Pierre-François Gimenez <pierre-francois.gimenez@centralesupelec.fr>
2022-08-19 13:49:14 +0200

    Angel of Darkness

--- main.lua
+++ main.lua
@@ -39,4 +39,39 @@ register_blueprint "challenge_nudity"
     },
 }
 
+register_blueprint "runtime_darkness"
+{
+    flags = { EF_NOPICKUP }, 
+    callbacks = {
+        on_enter_level = [=[
+            function ( self, entity, reenter )
+                local l = world:get_level()
+                l.level_info.light_range = 4
+                l.level_info.low_light = true
+			end
+		]=],
+    },
+}
+
+register_blueprint "challenge_darkness"
+{
+    text = {
+        name   = "Angel of Darkness",
+        desc   = "The world is a dark place.\n\nRating   : {GEASY}",
+        rating = "EASY",
+        abbr   = "AoD",
+        letter = "D",
+    },
+    challenge = {
+        type      = "challenge",
+    },
+    callbacks = {
+        on_create_player = [[
+            function( self, player )
+                player:attach( "runtime_darkness" )
+            end
+        ]],
+    },
+}
+
 
