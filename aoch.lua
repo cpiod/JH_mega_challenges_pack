bf4f4db47d164bdb39649f769e613c5322870a8a
cpiod <cpiod@pm.me>
2023-11-26 18:43:44 +0100

    Angel of Charon: summoned CRI will follow + bugfix level clear

--- aoch.lua
+++ aoch.lua
@@ -21,28 +21,26 @@ register_blueprint "trait_call_cri"
         on_use = [=[
             function ( self, player, level, target )
                 if self.attributes.kill < self.attributes.required_kills then
-					ui:set_hint( self.text.cant_use, 50, 1 )
-					return -1
+                    ui:set_hint( self.text.cant_use, 50, 1 )
+                    return -1
                 else
                     self.text.name = "Call CRI (0/15)"
                     self.attributes.kill = 0
-                    if not player:child( "cri_clearance" ) then
-                        player:attach( "cri_clearance" )
-                    end
                     local episode = world:get_level().level_info.episode
                     local ids     = { "cri_marine", "cri_bot",}
                     local count   = 2 * episode
+                    local positions = leveltk.find_open_elevator_coords( level )
                     for _=1,count do
                         local id = ids[ math.random( #ids ) ]
-                        local b  = world:create_entity( id )
-
+                        local pos = positions[math.random(#positions)]
+                        local b = level:add_entity( id, pos )
                         b.data.cri_clearanced = true
                         b.flags.data[ EF_BUMPACTION ] = true
-                        aitk.convert( b, world:get_player(), false, false, true, true )
+                        b.flags.data[ EF_FOLLOW ] = true
+                        aitk.convert( b, world:get_player(), false, false, true, true ) -- no control, no summon, no experience, no drop
                         b.data.ai.idle  = "active_hunt_no_player"
                         b.data.ai.state = "active_hunt_no_player"
                         b.attributes.experience_value = 0
-                        world:add_transfer( b )
                     end
                 end
             end
@@ -66,7 +64,40 @@ register_blueprint "trait_call_cri"
     },
 }
 
-
+register_blueprint "cpiod_cri_clearance"
+{
+    flags = { EF_NOPICKUP, EF_PERMANENT }, 
+    text = {
+        name  = "CRI Clearance",
+        desc  = "CRI has given you clearance. CRI forces will no longer attack you.",
+        bdesc = "non-corrupted CRI forces will be friendly",
+    },
+    callbacks = {
+        do_convert = [=[
+            function ( self, fx )
+                local l    = world:get_level()
+                for e in l:entities() do
+                    if e.data and e.data.cri and e.data.ai then
+                        local ai = e.data.ai
+                        if ai.group == "cri" or ai.group == "cri_n" then
+                            if fx then
+                                ui:spawn_fx( e, "fx_convert", e )
+                            end
+                            e.data.cri_clearanced = true
+                            e.flags.data[ EF_BUMPACTION ] = true
+                            aitk.convert( e, world:get_player(), false, false, false, true )
+                        end
+                    end
+                end
+            end
+        ]=],
+        on_enter_level = [=[
+            function ( self, entity, reenter )
+                world:lua_callback( self, "do_convert", false )
+            end
+        ]=],
+    },
+}
 
 register_blueprint "challenge_charon"
 {
@@ -87,6 +118,7 @@ register_blueprint "challenge_charon"
                 if egun then world:destroy( egun ) end
                 local eammo = player:child("ammo_9mm") or player:child("ammo_44") 
                 if eammo then world:destroy( eammo ) end
+                player:attach( "cpiod_cri_clearance" )
                 player:attach( "exo_blaster" )
                 player:equip( "armor_cri" )
                 player.equipment.count = 1
@@ -98,6 +130,4 @@ register_blueprint "challenge_charon"
         ]],
     },
 }
-
-
 
